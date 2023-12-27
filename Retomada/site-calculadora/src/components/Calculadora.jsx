41c6a0bfc4b017c2b2fd95f3f07f9649a8c957d6
Anselmo5 <anselmohenrique191@gmail.com>
2023-12-26 21:50:19 -0300

    update

--- Retomada/site-calculadora/src/components/Calculadora.jsx
+++ Retomada/site-calculadora/src/components/Calculadora.jsx
@@ -5,26 +5,7 @@ import './Calculadora.css'
 const Calculadora = () => {
   return (
     <div>
-       <div>
-        <nav>
-          <div className='containernav'>
-            <div className='alinglink'>
-              <img src={logo} alt="" />
-              <Link to='#' className='navlink active'>Home</Link>
-              <Link to='/repositorio' className='navlink'>Repositorio</Link>
-              <Link to='/calculadora' className='navlink'>Calculadora</Link>
-            </div>
-          </div>
-        </nav>
-
-
-        <footer>
-            <div className='footerinforme'>
-              <img src={logo} alt="" />
-              <h2>Informações e  dados rezervado</h2>
-            </div>
-        </footer>
-    </div>
+       
     </div>
   )
 }
--- Retomada/site-calculadora/src/components/Home.jsx
+++ Retomada/site-calculadora/src/components/Home.jsx
@@ -5,24 +5,7 @@ import logo from '../assets/logo.png'
 const Home = () => {
   return (
     <div>
-        <nav>
-          <div className='containernav'>
-            <div className='alinglink'>
-              <img src={logo} alt="" />
-              <Link className='navlink active'>Home</Link>
-              <Link to='/repositorio' className='navlink'>Repositorio</Link>
-              <Link to='/calculadora' className='navlink'>Calculadora</Link>
-            </div>
-          </div>
-        </nav>
-
-
-        <footer>
-            <div className='footerinforme'>
-              <img src={logo} alt="" />
-              <h2>Informações e  dados rezervado</h2>
-            </div>
-        </footer>
+       
     </div>
   )
 }
