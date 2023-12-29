334d62dd10d83bfdcfa6583fa90b2173ef45fd8f
Anselmo5 <anselmohenrique191@gmail.com>
2023-12-29 02:30:14 -0300

    update

--- Retomada/site-calculadora/src/components/Home.jsx
+++ Retomada/site-calculadora/src/components/Home.jsx
@@ -5,7 +5,41 @@ import logo from '../assets/logo.png'
 const Home = () => {
   return (
     <div>
-       
+    <nav>
+      <div className='alingconthome'>
+
+      <div className='formimg'>
+              <img src={logo} alt="" />
+            </div>
+
+        <div className='conthome'>
+                <Link className='active'>Home</Link>
+                <Link to='/repositorio'>Repositorio</Link>
+                <Link to='/calculadora'>Calculadora</Link>
+          </div>
+
+        </div>
+      </nav>
+
+
+        <div className='alingtexthome'>
+            <div className='texthome'>
+                <h2>Olá Pessoal</h2>
+                <p>Somos a empressa Calccode, <br/>nosso objetivo e calcular o lugto de venda da sua empressa no <br/> ano quantidade de vendas pelos dias ou anos <br/> que você deseja calcular</p>
+            </div>
+        </div>
+
+      <footer>
+          <div className='footerconthome'>
+              <div className='footerlogo'>
+                  <img src={logo} alt="" />
+              </div>
+
+              <div className='conttexthome'>
+                  <h2>Informações e  dados rezervados</h2>
+              </div>
+          </div>
+      </footer>
     </div>
   )
 }
--- Retomada/site-calculadora/src/components/Repositorio.jsx
+++ Retomada/site-calculadora/src/components/Repositorio.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import logogit from '../assets/gitlogo.png'
 import './Repositorio.css'
 const Repositorio = () => {
+
+
+  const handlerreferen = () => {
+    window.location.href = "https://github.com/Anselmo5/Ferias/tree/main/Retomada"
+  }
+
   return (
     <div>
-     
+         <nav>
+      <div className='alingcontrepo'>
+
+      <div className='formimgrepo'>
+              <img src={logo} alt="" />
+            </div>
+
+        <div className='contrepo'>
+                <Link to='/home'>Home</Link>
+                <Link to=''>Repositorio</Link>
+                <Link to='/calculadora'>Calculadora</Link>
+          </div>
+
+        </div>
+      </nav>
+
+        <div className='alingformrepo'>
+            <div className='formrepo'>
+                <div className='imgrepo'>
+                  <img src={logogit} alt="" />
+                </div>
+                <button onClick={() =>handlerreferen() }>ENTRAR</button>
+            </div>
+        </div>
+  <div className='footerrepo'>
+      <footer>
+          <div className='footercontrepo'>
+              <div className='footerlogorepo'>
+                  <img src={logo} alt="" />
+              </div>
+
+              <div className='conttextrepo'>
+                  <h2>Informações e  dados rezervados</h2>
+              </div>
+          </div>
+      </footer>
+  </div>
     </div>
   )
 }
