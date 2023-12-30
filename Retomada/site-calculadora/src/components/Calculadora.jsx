2a7d32f8acd1d3352612f26e2ff11cc0c0bd9aa0
Anselmo5 <anselmohenrique191@gmail.com>
2023-12-30 04:33:30 -0300

    update

--- Retomada/site-calculadora/src/components/Calculadora.jsx
+++ Retomada/site-calculadora/src/components/Calculadora.jsx
@@ -5,7 +5,41 @@ import './Calculadora.css'
 const Calculadora = () => {
   return (
     <div>
-       
+             <nav>
+      <div className='alingcontcalc'>
+
+      <div className='formimgcalc'>
+              <img src={logo} alt="" />
+            </div>
+    
+        <div className='contcalc'>
+                <Link to='/home'>Home</Link>
+                <Link to=''>Repositorio</Link>
+                <Link to='/calculadora'>Calculadora</Link>
+          </div>
+
+        </div>
+      </nav>
+
+        <div className='alingformcalc'>
+            <div className='formcalc'>
+                <button onClick={() =>handlerreferen() }>CALCULAR</button>
+            </div>
+        </div>
+
+  <div className='footercalc'>
+      <footer>
+          <div className='footercontcalc'>
+              <div className='footerlogocalc'>
+                  <img src={logo} alt="" />
+              </div>
+
+              <div className='conttextcalc'>
+                  <h2>Informações e  dados rezervados</h2>
+              </div>
+          </div>
+      </footer>
+  </div>
     </div>
   )
 }
