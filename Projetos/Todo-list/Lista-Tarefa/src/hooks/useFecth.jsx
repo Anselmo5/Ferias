3598b48c714930dccdcaf1c1a00761ad185f8826
Anselmo5 <anselmohenrique191@gmail.com>
2024-01-08 14:19:26 -0300

    update

--- Projetos/Todo-list/Lista-Tarefa/src/hooks/useFecth.jsx
+++ Projetos/Todo-list/Lista-Tarefa/src/hooks/useFecth.jsx
@@ -1,60 +1,55 @@
-// divição de funções, aqui e onde vai ficar as ações de adicionar ou excluir tarefa    
-import React,{ useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-const useFecth = (url) => {
-    const [data,setData] = useState(null)
-    const [config,setConfig] = useState(null) // configuração da requisição
-    const [method, setMethod] = useState(null) //metodo da requisição "get, post"
-    const [callfetch,setCallfetch] = useState(false) // atualização dos dados
-    const [loading,setloading] = useState(false)
-    const [error,setError] = useState(null)
+export const useFecth = (url) => {
+  const [data, setData] = useState(null);
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFecth, setCallFecth] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setErro] = useState(null);
 
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    const httpconfig = (data,method) => {
-        if(method === "POST"){
-            setConfig({
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data)
-            })
-            setMethod("POST")
-        }
+      setMethod("POST");
     }
+  };
 
-    useEffect(() =>{
-        const fectData = async () => {
-            setloading(true)
-            try {
-                const res = await fetch.url
-                const data = await res.json()
-                setData(data)
-            } catch (error) {
-            
-                setError('Houve um Erro')
-            }
-            setloading(false)           
-        }
-        fectData()
-    },[url,callfetch])
+  useEffect(() => {
+    const fecthData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setErro("Houve um erro no carregamento");
+      }
 
+      setLoading(false);
+    };
 
-    
-    useEffect(() =>{
-        const httpRequest = async () =>{
-            if (method === "POST"){
-                let fetchOptions = [url,config];
-                const res = await fetch(...fetchOptions)
-                const data = await res.json()
-                setCallfetch(data)
-            }
-        }
-        httpRequest()
-    },[config,method,url])
+    fecthData();
+  }, [url, callFecth]);
 
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        const res = await fetch(url, config);
+        const json = await res.json();
+        setCallFecth(json);
+      }
+    };
 
-    return{data,httpconfig,loading,error}
-}
+    httpRequest();
+  }, [config, method, url]);
 
-export default useFecth
+  return { data, httpConfig, loading, error };
+};
