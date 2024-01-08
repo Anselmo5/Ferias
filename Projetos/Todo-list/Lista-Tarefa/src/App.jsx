945aa352d202cbc9f329fd08f2fdfca4b0eb4d64
Anselmo5 <anselmohenrique191@gmail.com>
2024-01-08 11:40:10 -0300

    update

--- Projetos/Todo-list/Lista-Tarefa/src/App.jsx
+++ Projetos/Todo-list/Lista-Tarefa/src/App.jsx
@@ -1,14 +1,92 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Home from './components/home.jsx'
+import useFecth from './hooks/useFecth.jsx'
+import git from './assets/git.png'
+import instagram from './assets/instagram.png'
+import linkedin from './assets/linkedin.png'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
+    const url = 'http://localhost:3000/Tarefas'
+    const [Tarefas,setTarefas] = useState()
+    const [hor,setHor] = useState()
+    const [name,setName] = useState()
+    const {data:items,httpconfig,loading,error} = useFecth(url)
+
+    const enviotarefa = async (event) => {
+        event.preventDefault()
+
+        const tarefa ={
+          name,
+          hor,
+        }
+
+        httpconfig(tarefa,"POST")
+        setName("")
+        setHor("")
+    }
+
+
+
 
   return (
     <>
-        <Home/>
+        <div className='alingcontainer'>
+                 <div className=' mensege'>
+                  <h2>LISTA DE TAREFAS</h2>
+                 </div>
+            <div className='container'>
+                <div>
+                <div className='midiaaling'>
+                      <div className='formmidia'>
+                         <Link to='https://www.instagram.com/anselmo_henrique02/?next=%2F' target='_blank'> <img src={instagram} alt="" /></Link>
+                         <Link to='https://www.linkedin.com/in/anselmo-henrique-266647233/' target='_blank'> <img src={linkedin} alt="" /></Link>
+                         <Link to='https://github.com/Anselmo5' target='_blank'> <img src={git} alt="" /></Link>
+                      </div>
+                  </div>
+
+                  <div className='alingenvio'>
+                      <div className='tarefaenvio'>
+                          <input type="text" />
+                          <button onClick={() => enviotarefa()}> ADICIONAR</button>
+                      </div>
+                  </div>
+
+                  <div className='alingtarefas'>
+                      <div className='tarefas'>
+                        <h3>TAREFAS</h3>
+
+
+                {loading &&
+                  <div class="wrapper">
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="shadow"></div>
+                    <div class="shadow"></div>
+                    <div class="shadow"></div>
+                  </div>                
+                }
+                {error&& <p>{error}</p>}
+
+                {!loading &&   
+                  <ul className='form'>
+                      {items && items.map((tarefa) => (  //sintaxe do uso do map
+                        <li key={tarefa.id} className='form2'>
+                          {tarefa.name} R$: {tarefa.hor} 
+                        </li>
+                      ))}
+                    </ul>
+                    }
+
+                      </div>
+                  </div>
+                </div>
+            </div>
+         </div>
     </>
   )
 }
 
-export default App
+export default App 
--- Projetos/Todo-list/Lista-Tarefa/src/hooks/useFecth.jsx
+++ Projetos/Todo-list/Lista-Tarefa/src/hooks/useFecth.jsx
@@ -1,1 +1,60 @@
 // divição de funções, aqui e onde vai ficar as ações de adicionar ou excluir tarefa    
+import React,{ useState,useEffect } from 'react'
+
+const useFecth = (url) => {
+    const [data,setData] = useState(null)
+    const [config,setConfig] = useState(null) // configuração da requisição
+    const [method, setMethod] = useState(null) //metodo da requisição "get, post"
+    const [callfetch,setCallfetch] = useState(false) // atualização dos dados
+    const [loading,setloading] = useState(false)
+    const [error,setError] = useState(null)
+
+
+    const httpconfig = (data,method) => {
+        if(method === "POST"){
+            setConfig({
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            })
+            setMethod("POST")
+        }
+    }
+
+    useEffect(() =>{
+        const fectData = async () => {
+            setloading(true)
+            try {
+                const res = await fetch.url
+                const data = await res.json()
+                setData(data)
+            } catch (error) {
+            
+                setError('Houve um Erro')
+            }
+            setloading(false)           
+        }
+        fectData()
+    },[url,callfetch])
+
+
+    
+    useEffect(() =>{
+        const httpRequest = async () =>{
+            if (method === "POST"){
+                let fetchOptions = [url,config];
+                const res = await fetch(...fetchOptions)
+                const data = await res.json()
+                setCallfetch(data)
+            }
+        }
+        httpRequest()
+    },[config,method,url])
+
+
+    return{data,httpconfig,loading,error}
+}
+
+export default useFecth
